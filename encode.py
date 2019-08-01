d9a0aa2d51c9641a7c511b3758d7df4b589164c7
nick <nick33omori@gmail.com>
2019-08-01 14:39:44 -0300

    batch training the svm, outputs on the output folder

--- encode.py
+++ encode.py
@@ -43,22 +43,38 @@ def is_normal(vector, normal_traces):
     # if true, it is a normal behaviour
     return " ".join(vector) in normal_traces
 
+def apply_svm(log_name, normal_traces):
+    vectors = get_traces("logs/" + log_name + ".csv")
+    v = create_vectors("models_size16/" + log_name + ".model", vectors)
+    output_file = open("outputs/" + log_name + ".txt", "w+")
+    nus = [0.05, 0.1, 0.3, 0.5]
+
+    for nu in nus:
+        print(nu, file=output_file)
+        ocsvm = OneClassSVM(kernel='linear', nu=nu)
+        classes = list(ocsvm.fit_predict(v))
+        print(classes.count(-1), classes.count(1), file=output_file)
+
+        anom_cases = [vectors[k] for k, x in enumerate(classes) if x == -1]
+        normal_cases = [vectors[k] for k, x in enumerate(classes) if x == 1]
+
+        anom_checked = [is_normal(t, normal_traces) for t in anom_cases]
+        normal_checked = [is_normal(t, normal_traces) for t in normal_cases]
+        # print([k for k, x in enumerate(classes) if x == -1], [k for k, x in enumerate(classes) if x == 1])
+        print("size:", len(anom_checked), "normal:", anom_checked.count(True), "anomaly:", anom_checked.count(False), file=output_file)
+        print("size:", len(normal_checked), "normal:", normal_checked.count(True), "anomaly:", normal_checked.count(False), file=output_file)
+
+
+def apply_svm_batch():
+    normal_traces1 = [line.strip('\n') for line in open("normal_pn1.txt", 'r')]
+    normal_traces2 = [line.strip('\n') for line in open("normal_pn2.txt", 'r')]
+    for filename in os.listdir("logs"):
+        filename = re.sub(r"\.csv$", "", filename)
+        if filename.startswith("log1"):
+            apply_svm(filename, normal_traces1)
+        elif filename.startswith("log2"):
+            apply_svm(filename, normal_traces2)
+
 
 # train_batch()
-vectors = get_traces("logs/log1_anom_all_5.csv")
-v = create_vectors("models/log1_anom_all_5.model", vectors)
-
-ocsvm = OneClassSVM(kernel='rbf', nu=0.05)
-classes = list(ocsvm.fit_predict(v))
-print(classes.count(-1), classes.count(1))
-
-anom_cases = [vectors[k] for k, x in enumerate(classes) if x == -1]
-normal_cases = [vectors[k] for k, x in enumerate(classes) if x == 1]
-
-normal_traces = [line.strip('\n') for line in open("normal_pn1.txt", 'r')]
-
-anom_checked = [is_normal(t, normal_traces) for t in anom_cases]
-normal_checked = [is_normal(t, normal_traces) for t in normal_cases]
-# print([k for k, x in enumerate(classes) if x == -1], [k for k, x in enumerate(classes) if x == 1])
-print("size:", len(anom_checked), "normal:", anom_checked.count(True), "anomaly:", anom_checked.count(False), [case for case in anom_cases if is_normal(case, normal_traces)])
-print("size:", len(normal_checked), "normal:", normal_checked.count(True), "anomaly:", anom_checked.count(False))
+apply_svm_batch()

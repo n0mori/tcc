5b22cb7043c26fce623116dc72f27d8767b82ae2
n0mori <nick33omori@gmail.com>
2019-08-20 10:55:00 -0300

    average on the outputs

--- merge_outputs.py
+++ merge_outputs.py
@@ -4,6 +4,7 @@ import csv
 
 fields = ['log', 'method', 'nu', 'accuracy']
 
+
 def calculate_accuracy(ifile, ofile):
     with open(ifile, "r") as csvfile, open(ofile, "w+") as output:
         reader = csv.reader(csvfile, delimiter=',')
@@ -14,9 +15,25 @@ def calculate_accuracy(ifile, ofile):
             fn = float(line[7])
             tn = float(line[6])
             accuracy = (tp + tn) / (tp + tn + fp + fn)
-            print(line[0], line[1], line[8], round(accuracy, 2), sep=',', file=output)
+            print(line[0], line[1], line[8], round(accuracy, 2),
+                  sep=',', file=output)
 
 
-for i in range(1,11):
+def average():
     dir = "outputs/"
-    calculate_accuracy(dir + "output" + str(i) + ".csv", dir + "acc" + str(i) + ".csv")
+    files = [open(f"{dir}acc{i}.csv", "r") for i in range(1, 11)]
+    readers = [csv.reader(f, delimiter=',') for f in files]
+    with open(f"{dir}avg.csv", "w+") as output:
+        while True:
+            try:
+                rows = [next(row) for row in readers]
+            except StopIteration:
+                break
+            accuracies = [float(r[3]) for r in rows]
+            method = rows[0][1] + rows[0][2] if rows[0][1] == "oc" else rows[0][1]
+            average = sum(accuracies) / len(accuracies)
+            average = round(average, 2)
+            print(rows[0][0], method, average, sep=',', file=output)
+
+
+average()
